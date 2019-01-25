16959bb2299d6cca31e7676eefb6db0eb2b2cbff
RohanPK <f20170118@goa.bits-pilani.ac.in>
2019-01-25 17:15:01 +0530

    Update with master

---
+++ cameradata/ball_detect/ball_detect.py
@@ -0,0 +1,175 @@
+import freenect
+import pickle
+from operator import itemgetter
+from time import sleep
+
+import cv2
+import imutils
+import numpy as np
+
+from cameradata.utils.perspTransform import four_point_transform
+
+
+def get_video(pts):
+    array, _ = freenect.sync_get_video()
+    array = cv2.cvtColor(array, cv2.COLOR_RGB2BGR)
+    return four_point_transform(array, pts)
+
+
+def get_depth(pts):
+    array, _ = freenect.sync_get_depth()
+    array = array.astype(np.uint8)
+    return four_point_transform(array, pts)
+
+
+def get_ball_reference(pts_depth, pts_rgb):
+    """Frame with no balls and no cue"""
+    img_depth = get_depth(pts_depth)
+    img = get_video(pts_rgb)
+    return img_depth, img
+
+
+def get_ball_diff(curr_depth, Ra):
+    return cv2.absdiff(curr_depth, Ra)
+
+
+def get_ball_contours(curr_depth, Ra):
+    ballFrame = get_ball_diff(curr_depth, Ra)
+    # cv2.imshow("2", imutils.resize(ballFrame, height=320))
+
+    Bw = 26
+    Tb = (13 / 16) * Bw
+    ballBin = np.zeros(ballFrame.shape, np.uint8)
+    ballBin[ballFrame > Tb] = 255
+
+
+    # ballSmooth = cv2.medianBlur(ballBin, 5)
+    ballErode = cv2.erode(ballBin, np.ones((7, 7), np.uint8))
+    # cv2.imshow("4", imutils.resize(ballErode, height=320))
+
+    contours, hierarchy = cv2.findContours(ballErode, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
+
+
+def draw_contour_circles(img, contours):
+    n = 0
+    centers_all = []
+    radii_all = []
+    for i in range(len(contours)):
+        if cv2.contourArea(contours[i]) > 110:
+            (x, y), radius = cv2.minEnclosingCircle(contours[i])
+            center = (int(x), int(y))
+            centers_all.append(center)
+            radius = int(radius)
+            radii_all.append(radius)
+            img = cv2.drawContours(img, contours, i, 255, 1)
+            img = cv2.circle(img, center, radius + 3, 255, 1)
+            img = cv2.rectangle(img, (int(x - 1), int(y - 1)), (int(x + 1), int(y + 1)), 255)
+            n += 1
+    return img, centers_all, radii_all
+
+
+def get_white_ball(img_rgb, img_depth, contours):
+    global wht_center, wht_radius
+    img_rgb = cv2.resize(img_rgb, (img_depth.shape[1], img_depth.shape[0]))
+    # Initialize empty list
+    # global max_cnt_index, max_cnt
+    lst_intensities = []
+
+    # For each list of contour points...
+
+    for i in range(len(contours)):
+        if cv2.contourArea(contours[i]) > 110:
+            # Create a mask image that contains the contour filled in
+            cimg = np.zeros_like(img_rgb)
+            cimg = cv2.resize(cimg, (img_depth.shape[1], img_depth.shape[0]))
+            cv2.drawContours(cimg, contours, i, color=255, thickness=-1)
+
+            # Access the image pixels and create a 1D numpy array then add to list
+            pts = np.where(cimg == 255)
+            lst_intensities.append((i, img_rgb[pts[0], pts[1]]))
+
+    # print(lst_intensities)
+
+    sum_val = []
+    Sum = 0
+    for i, cnt in lst_intensities:
+        for val in cnt:
+            # print(val)
+            Sum += sum(val)
+        sum_val.append((i, Sum))
+        Sum = 0
+    # print(sum_val)
+    wht_center = (0, 0)
+    wht_radius = 0
+    max_cnt_index = None
+    max_cnt = None
+    if len(sum_val) > 0:
+        max_cnt_index = max(sum_val, key=itemgetter(1))[0]
+        cv2.drawContours(img_rgb, contours, max_cnt_index, color=(255, 120, 0), thickness=-1)
+        max_cnt = contours[max_cnt_index]
+
+    # print(max_cnt)
+    if max_cnt is not None and cv2.contourArea(max_cnt) > 110:
+        (x, y), radius = cv2.minEnclosingCircle(max_cnt)
+        wht_center = (int(x), int(y))
+        wht_radius = int(radius)
+
+    return max_cnt_index, max_cnt, wht_center, wht_radius, img_rgb
+
+
+def ball_det(curr_frame_depth, curr_frame_rgb, Ra):
+    global cent_depth, cent_rgb
+
+    cent_depth, cent_rgb = [], []
+    rad_depth, rad_all = [], []
+    contours = get_ball_contours(curr_frame_depth, Ra)
+
+    img_depth = curr_frame_depth
+    img_rgb = curr_frame_rgb
+
+    img_rgb = cv2.resize(img_rgb, (img_depth.shape[1], img_depth.shape[0]))
+    # image1 = cv2.drawContours(image1, contours, -1, 255, 2)
+
+    img_depth, cent_all, rad_all = draw_contour_circles(img_depth, contours)
+    img_rgb, cent_rgb_all, cent_rgb_all = draw_contour_circles(img_rgb, contours)
+
+    #cv2.imshow("5", imutils.resize(img_depth, height=320))
+    #cv2.imshow("6", imutils.resize(img_rgb, height=320))
+    # noinspection PyTypeChecker
+    max_cnt_idx, max_cnt, wht_cent, wht_rad, img_rgb = get_white_ball(img_rgb, img_depth=img_depth, contours=contours)
+
+    cent_depth.append(wht_cent)
+    rad_depth.append(wht_rad)
+    for cent in cent_all:
+        if cent != cent_depth[0]:
+            cent_depth.append(cent)
+    for rad in rad_all:
+        if rad != rad_depth[0]:
+            rad_depth.append(rad)
+
+    return cent_depth, rad_depth
+
+if __name__ == "__main__":
+    with open('../sys_setup/pts_depth.pkl', 'rb') as input1:
+        pts_depth = pickle.load(input1)
+
+    with open('../sys_setup/pts_rgb.pkl', 'rb') as input2:
+        pts_rgb = pickle.load(input2)
+
+    x = get_depth(pts_depth)
+    Ra = cv2.imread('../sys_setup/ref_depth_no_balls.png')
+    Ra = Ra[:, :, 0]
+    Ra_rgb = cv2.imread('../sys_setup/ref_rgb_no_balls.png')
+
+    while 1:
+
+        curr_frame_depth, curr_frame_rgb = get_depth(pts_depth), get_video(pts_rgb)
+        cent_depth, rad_depth = ball_det(curr_frame_depth, curr_frame_rgb, Ra)
+        print(len(cent_depth))
+        print(cent_depth[0])
+
+        k = cv2.waitKey(5) & 0xFF
+        if k == 27:
+            break
+    cv2.destroyAllWindows()
---
+++ cameradata/cue_detect/cue_detect.py
@@ -0,0 +1,233 @@
+import freenect
+import cv2
+import numpy as np
+import imutils
+from cameradata.utils.perspTransform import four_point_transform
+from cameradata.utils.get_pts_gui import get_points
+from time import sleep
+from operator import itemgetter
+from corelogic.ball_class import get_wall_collisions
+import pickle
+
+
+def get_video(pts):
+    array, _ = freenect.sync_get_video()
+    array = cv2.cvtColor(array, cv2.COLOR_RGB2BGR)
+    return four_point_transform(array, pts)
+
+
+def get_depth(pts):
+    array, _ = freenect.sync_get_depth()
+    array = array.astype(np.uint8)
+    return four_point_transform(array, pts)
+
+
+def get_cue_reference(pts):
+    """Frame with balls and no cue"""
+    img = get_depth(pts)
+    return img
+
+
+def get_cue_diff(curr_frame_depth, Qa):
+    return cv2.absdiff(curr_frame_depth, Qa)
+
+
+def max_cnt_area(contours):
+    maxArea = 0
+    maxCnt = None
+
+    for cnt in contours:
+        if cv2.contourArea(cnt) > maxArea:
+            maxArea = cv2.contourArea(cnt)
+            maxCnt = cnt
+
+    return maxArea, maxCnt
+
+
+def get_avg_area(contours):
+    Al, _ = max_cnt_area(contours)
+    Nc = len(contours)
+    avg = 0
+    for cnt in contours:
+        if cv2.contourArea(cnt) < Al:
+            avg += cv2.contourArea(cnt)
+
+    avg -= Al
+    print(avg)
+    avg /= Nc - 1
+
+    return abs(100 * avg)
+
+
+def get_req_cnts(contours):
+    req_cnt = []
+    for cnt in contours:
+        if cv2.contourArea(cnt) < get_avg_area(contours):
+            req_cnt.append(cnt)
+    return req_cnt
+
+
+def get_req_contours(contours):
+    cnt_ar = []
+    for cnt in contours:
+        cnt_ar.append((cnt, cv2.contourArea(cnt)))
+
+    sort_cnt_ar = sorted(cnt_ar, key=itemgetter(1), reverse=True)
+    req_cnt = []
+    for cnt, ar in sort_cnt_ar:
+        if ar > 170:
+            req_cnt.append(cnt)
+    return req_cnt
+
+
+def get_HoughLines(thres_img):
+    lines = cv2.HoughLines(thres_img, 1, 5 * np.pi / 180, 80)
+
+    avg_x1 = 0
+    avg_x2 = 0
+    avg_y1 = 0
+    avg_y2 = 0
+    n = 0
+    if lines is not None:
+        for line in lines[:15]:
+            for rho, theta in line:
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
+                x1 = int(x0 + 1000 * (-b))
+                y1 = int(y0 + 1000 * (a))
+                x2 = int(x0 - 1000 * (-b))
+                y2 = int(y0 - 1000 * (a))
+                avg_x1 += x1
+                avg_x2 += x2
+                avg_y1 += y1
+                avg_y2 += y2
+                n += 1
+
+                #cv2.line(img, (x1, y1), (x2, y2), 255, 2)
+
+        avg_x1 = int(avg_x1 / n)
+        avg_x2 = int(avg_x2 / n)
+        avg_y1 = int(avg_y1 / n)
+        avg_y2 = int(avg_y2 / n)
+
+    return [(avg_x1,avg_y1), (avg_x2, avg_y2)]
+
+
+def get_HoughLinesP(thres_img):
+    minLineLength = 50
+    maxLineGap = 15
+    lines = cv2.HoughLinesP(thres_img, 1, np.pi / 180, 80, minLineLength, maxLineGap)
+
+    avg_x1 = 0
+    avg_x2 = 0
+    avg_y1 = 0
+    avg_y2 = 0
+    n = 0
+    if lines is not None:
+        for line in lines[:]:
+            for x1, y1, x2, y2 in line:
+                avg_x1 += x1
+                avg_x2 += x2
+                avg_y1 += y1
+                avg_y2 += y2
+                n += 1
+                #cv2.line(img, (x1, y1), (x2, y2), 0, 1)
+
+        avg_x1 = int(avg_x1 / n)
+        avg_x2 = int(avg_x2 / n)
+        avg_y1 = int(avg_y1 / n)
+        avg_y2 = int(avg_y2 / n)
+
+        return [(avg_x1, avg_y1), (avg_x2, avg_y2)]
+
+
+def cue_det(curr_frame_depth, pts_depth, Qa, M, N, wht_center):
+    global reflections, cue_line
+
+    cueFrame = get_cue_diff(curr_frame_depth, Qa)
+    # cv2.imshow("2", imutils.resize(cueFrame, height=320))
+
+    Tc = 0.0001 * 65535
+    cueBin = np.zeros(cueFrame.shape, np.uint8)
+    cueBin[cueFrame > Tc] = 255
+    # cv2.imshow("3", imutils.resize(cueBin, height=320))
+
+    contours, hierarchy = cv2.findContours(cueBin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    cnt_image = np.zeros(cueFrame.shape, np.uint8)
+    cnt_image_2 = cnt_image.copy()
+    #cnt_image_all = cv2.drawContours(cnt_image, contours, -1, 255, 1)
+
+    # cv2.imshow("4", imutils.resize(cnt_image_all, height=320))
+
+    req_cnt = get_req_contours(contours)
+    cnt_image_req = cv2.drawContours(cnt_image_2, req_cnt, -1, 255, 1)
+    kernel = np.ones((8, 8), np.uint8)
+    closing = cv2.morphologyEx(cnt_image_req, cv2.MORPH_CLOSE, kernel)
+    #cv2.imshow("5", imutils.resize(closing, height=320))
+    # print(len(contours), ",", len(req_cnt))
+
+    img = get_depth(pts_depth)
+    img_final = img.copy()
+    # img_final = cv2.resize(img_final, (cueFrame.shape[1], cueFrame.shape[0]))
+
+    linesP = get_HoughLinesP(closing)
+
+    lines = get_HoughLines(closing)
+    cue_line = lines
+    #cv2.imshow("6", imutils.resize(img, height=320))
+
+    #cv2.line(img_final, lines[0], lines[1], 255, 1)
+    wht_center_x = wht_center[0]
+    wht_center_y = wht_center[1]
+    try:
+        reflections = get_wall_collisions(4, 12, M, N, wht_center_x, wht_center_y, cue_h_prob=linesP, cue_h_norm=lines)
+    except ZeroDivisionError:
+        reflections = []
+
+    #for i in range(1, len(reflections)):
+        #cv2.line(img_final, reflections[i - 1], reflections[i], 0, 2)
+        #x = reflections[i - 1][0]
+        #y = reflections[i - 1][1]
+        #cv2.circle(img_final, (x, y), 12, 0, -1)
+        #print(2, 12, M, N, 136, 114, linesP, lines)
+        #print(x, y)
+
+    #cv2.imshow("7", imutils.resize(img_final, height=320))
+    return reflections, cue_line
+
+
+if __name__ == "__main__":
+
+    with open('../../sys_setup/pts_depth.pkl', 'rb') as input:
+        pts_depth = pickle.load(input)
+
+    with open('../../sys_setup/pts_rgb.pkl', 'rb') as input2:
+        pts_rgb = pickle.load(input2)
+
+    M = int(pts_depth[1][0] - pts_depth[0][0])
+    N = abs(int(pts_depth[2][0] - pts_depth[0][0]))
+    Qa = get_cue_reference(pts_depth)
+    # cv2.imshow("1", imutils.resize(Qa, height=320))
+    imgrgb = get_video(pts_rgb)
+    wht_center = (177, 105)
+    while 1:
+        curr_frame_depth = get_depth(pts_depth)
+        img_final = curr_frame_depth.copy()
+        reflections, cue_line = cue_det(curr_frame_depth, pts_depth, Qa, M, N, wht_center)
+
+        for i in range(1, len(reflections)):
+            cv2.line(img_final, reflections[i-1], reflections[i], 0, 2)
+            x = reflections[i-1][0]
+            y = reflections[i-1][1]
+            cv2.circle(img_final, (x, y), 12, 0, -1)
+        if len(reflections) > 0:
+            cv2.line(img_final, cue_line[0], cue_line[1], 0, 2)
+        cv2.imshow("7", imutils.resize(img_final, height=320))
+
+        k = cv2.waitKey(5) & 0xFF
+        if k == 27:
+            break
+    cv2.destroyAllWindows()
